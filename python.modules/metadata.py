927625432238edc04456b88ba73246cce63a4b7c
Robert Citek <robert.citek@gmail.com>
2023-10-31 21:30:02 -0600

    Update metadata.py

--- python.modules/metadata.py
+++ python.modules/metadata.py
@@ -3,7 +3,16 @@ import pandas as pd
 def metadata( dataframe ):
   '''Given a dataframe, returns a dataframe of metadata about the dataframe'''
   metatdata_df = dataframe.describe( include = "all" ).transpose()
-  metatdata_df = metatdata_df.astype( {"count": int }).rename( columns = {"50%": "median"})
+  metatdata_df = (
+    metatdata_df
+    .astype( { "count" : int } )
+    .rename( columns = {
+      "25%" : "Q1_25%",
+      "50%" : "Q2_median",
+      "75%" : "Q3_75%",
+      }
+    )
+  )
   metatdata_df["unique"] = dataframe.nunique()
   metatdata_df["Nulls"] = dataframe.isnull().sum()
   metatdata_df["Nulls_pct"] = ( metatdata_df["Nulls"] / dataframe.shape[0] * 100 ).round(1)
@@ -16,4 +25,14 @@ def cols_to_drop( dataframe ):
   md = metadata( dataframe )
   filter = md["Nulls_pct"] >= 80
   return md[ filter ]["Nulls_pct"].to_dict()
+
+def likely_ids( dataframe ):
+  '''Given a dataframe, returns a dictionary of likely ID columns'''
+  md = metadata( dataframe )
+  ids = ( md["unique"]/md["count"] ) * 100
+  return  ids[ ids > 95 ].sort_values( ascending = False).to_dict()
+
+
+
+
 
