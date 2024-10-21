40f5a8ac937133f3181391c3a351af0456d799d6
Robert Citek <robert.citek@gmail.com>
2024-10-20 23:52:46 -0600

    refactor metadata() and likely_id() function

--- python.modules/metadata.py
+++ python.modules/metadata.py
@@ -2,9 +2,17 @@ import pandas as pd
 
 def metadata( dataframe ):
   '''Given a dataframe, returns a dataframe of metadata about the dataframe'''
-  metatdata_df = dataframe.describe( include = "all" ).transpose()
-  metatdata_df = (
-    metatdata_df
+  metadata_df = pd.DataFrame()
+  metadata_df["Nulls"] = dataframe.isnull().sum()
+  metadata_df.insert(0, "Count", len(dataframe))
+  metadata_df["Nulls_pct"] = ( metadata_df["Nulls"] / metadata_df["Count"] * 100 ).round(1)
+  metadata_df["Data_types"] = dataframe.dtypes
+  metadata_df["Memory"] = dataframe.memory_usage( deep = True)
+  metadata_df["NUnique"] = dataframe.nunique()
+  metadata_df["NUnique_pct"] = (dataframe.nunique() / metadata_df["Count"] * 100).round(1)
+  metadata_df = metadata_df.join( dataframe.describe( include = "all" ).transpose() )
+  metadata_df = (
+    metadata_df
     .astype( { "count" : int } )
     .rename( columns = {
       "25%" : "Q1_25%",
@@ -13,24 +21,18 @@ def metadata( dataframe ):
       }
     )
   )
-  metatdata_df["unique"] = dataframe.nunique()
-  metatdata_df["Nulls"] = dataframe.isnull().sum()
-  metatdata_df["Nulls_pct"] = ( metatdata_df["Nulls"] / dataframe.shape[0] * 100 ).round(1)
-  metatdata_df["Data_types"] = dataframe.dtypes
-  metatdata_df["Memory"] = dataframe.memory_usage( deep = True)
-  return metatdata_df
+  return metadata_df
   
 def cols_to_drop( dataframe ):
   '''Given a dataframe, returns columns that should likely be dropped'''
   md = metadata( dataframe )
-  filter = md["Nulls_pct"] >= 80
+  filter = md["Nulls_pct"] >= 40
   return md[ filter ]["Nulls_pct"].to_dict()
 
 def likely_ids( dataframe ):
   '''Given a dataframe, returns a dictionary of likely ID columns'''
   md = metadata( dataframe )
-  ids = ( md["unique"]/md["count"] ) * 100
-  return  ids[ ids > 95 ].sort_values( ascending = False).to_dict()
+  return md["NUnique_pct"][ md["NUnique_pct"] > 95 ].sort_values( ascending = False )
 
 
 
