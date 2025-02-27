7c7c8baca2a136a43dd374da717844182fee2450
Robert Citek <robert.citek@gmail.com>
2025-02-27 07:26:35 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='metadata',  # Name of the module
+    name='python_modules',  # Name of the module
     version='0.1',
     packages=find_packages(),
     install_requires=[],  # Add any dependencies here
