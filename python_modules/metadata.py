3e4c66be122c4166da01660010500cb2749174d9
Robert Citek <robert.citek@gmail.com>
2025-03-06 23:59:03 -0700

    Update metadata.py

--- python_modules/metadata.py
+++ python_modules/metadata.py
@@ -3,10 +3,10 @@ import pandas as pd
 def metadata( dataframe ):
   '''Given a dataframe, returns a dataframe of metadata about the dataframe'''
   metadata_df = pd.DataFrame()
-  metadata_df["Nulls"] = dataframe.isnull().sum()
-  metadata_df.insert(0, "Count", len(dataframe))
-  metadata_df["Nulls_pct"] = ( metadata_df["Nulls"] / metadata_df["Count"] * 100 ).round(1)
   metadata_df["Data_types"] = dataframe.dtypes
+  metadata_df["Count"] = len(dataframe))
+  metadata_df["Nulls"] = dataframe.isnull().sum()
+  metadata_df["Nulls_pct"] = ( metadata_df["Nulls"] / metadata_df["Count"] * 100 ).round(1)
   metadata_df["Memory"] = dataframe.memory_usage( deep = True)
   metadata_df["NUnique"] = dataframe.nunique()
   metadata_df["NUnique_pct"] = (dataframe.nunique() / metadata_df["Count"] * 100).round(1)
